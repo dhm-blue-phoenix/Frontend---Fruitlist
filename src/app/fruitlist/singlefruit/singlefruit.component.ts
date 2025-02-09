20d6858cff6cfb6ae452f8ef8bc1f4396784b525
dhm-blue-phoenix <phoenix.dh-m16@outlook.com>
2025-02-09 16:42:20 +0100

    update fruitlist

---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss'
+})
+
+export class SinglefruitComponent {
+  @Input() fruit = {
+    name: "Apfel",
+    img: "apple.png",
+    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+    stars: 2.3,
+    reviews: [{ name: "Kevin W.", text: "ist lecker" }, { name: "Arne P.", text: "nicht so meins" }],
+  };
+
+  @Output() fruitname = new EventEmitter<string>();
+
+  inputData: string = "";
+
+  sendInputData() {
+    this.fruitname.emit(this.inputData);
+    this.inputData = "";
+  };
+};
